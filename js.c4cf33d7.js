96407939dbcaf27229f3a69bee78a6a9a7e04fb5
PavelKreitsberg <99120090+PavelKreitsberg@users.noreply.github.com>
2022-05-24 11:32:20 +0000

    Deploying to gh-pages from @ PavelKreitsberg/team-project-html-css@43fbb4d5c4098be6b31323f4cb8d38bb306f8899 🚀

--- js.c4cf33d7.js
+++ js.c4cf33d7.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"QvaY":[function(require,module,exports) {
 "use strict";require("../sass/main.scss");
 },{"../sass/main.scss":"clu1"}]},{},["QvaY"], null)
-//# sourceMappingURL=/team-project-html-css/js.08279cba.js.map
+//# sourceMappingURL=/team-project-html-css/js.c4cf33d7.js.map
